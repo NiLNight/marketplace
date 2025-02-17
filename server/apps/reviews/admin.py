7f166c9dddb0e4e5eb476d7cf2290955dc6dddf5
NiLNight <bkmz-2020@inbox.ru>
2025-02-17 23:19:59 +0300

    модель рейтинга товара

--- server/apps/reviews/admin.py
+++ server/apps/reviews/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Rating
 
-# Register your models here.
+
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ('product', 'user', 'value', 'time_create', 'ip_address')
+    list_filter = ('value', 'time_create')
+    search_fields = ('product__title', 'user__username', 'ip_address')
--- server/apps/reviews/models.py
+++ server/apps/reviews/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+from apps.products.models import Product
+
+
+class Rating(models.Model):
+    """
+    Модель рейтинга: От 1 до 5
+    """
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='ratings', verbose_name='Запись')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, verbose_name='Пользователь')
+    value = models.BigIntegerField(default=True, verbose_name='Значение')
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время добавления')
+    ip_address = models.GenericIPAddressField(verbose_name='IP Адрес')
+
+    class Meta:
+        unique_together = ('product', 'ip_address')
+        ordering = ['-time_create']
+        indexes = [models.Index(fields=['-time_create', 'value'])]
+        verbose_name = 'Рейтинг'
+        verbose_name_plural = 'Рейтинги'
+
+    def __str__(self):
+        return self.product.title
