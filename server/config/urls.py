9baa14111db0ad0bc74b3fc6d46e0cc75fe393e7
NiLNight <bkmz-2020@inbox.ru>
2025-02-17 21:15:37 +0300

    добавление путей products

--- server/config/urls.py
+++ server/config/urls.py
@@ -8,6 +8,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, Spec
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('apps.users.urls')),
+    path('products/', include('apps.products.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger'),
     path('api/docs/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
