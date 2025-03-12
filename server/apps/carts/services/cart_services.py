9b9181fe567768d80e81634fdc20970e03551679
NiLNight <bkmz-2020@inbox.ru>
2025-03-12 16:39:55 +0300

    Сервисный слой для создания корзины

--- server/apps/carts/services/cart_services.py
+++ server/apps/carts/services/cart_services.py
@@ -1,60 +1,25 @@
 from django.db import transaction
-from django.core.exceptions import ObjectDoesNotExist
-from apps.orders.models import OrderItem
+from django.core.exceptions import ValidationError
+from apps.carts.models import OrderItem
 from apps.products.models import Product
-from apps.carts.exceptions import *
+from apps.users.models import User
 
 
 class CartService:
-    MAX_ITEMS = 40
-    MAX_QUANTITY = 20
-
-    @classmethod
-    def get_cart_queryset(cls, request):
-        """Получение QuerySet для корзины"""
-        user = request.user if request.user.is_authenticated else None
-        session_key = request.session.session_key
-
-        if user:
-            return OrderItem.objects.filter(user=user, order=None)
-        elif session_key:
-            return OrderItem.objects.filter(session_key=session_key, order=None)
-        return OrderItem.objects.none()
-
-    @classmethod
-    def _validate_product(cls, product):
-        if not product.is_available or product.stock < 1:
-            raise ProductNotAvailable()
-
-    @classmethod
-    def _validate_quantity(cls, quantity):
-        if not (1 <= quantity <= cls.MAX_QUANTITY):
-            raise InvalidQuantity(f"Количество должно быть от 1 до {cls.MAX_QUANTITY}")
-
-    @classmethod
-    def _get_identifier(cls, request):
-        if request.user.is_authenticated:
-            return {'user': request.user}
-        return {'session_key': request.session.session_key}
-
-    @classmethod
+    @staticmethod
     @transaction.atomic
-    def add_to_cart(cls, request, product_id, quantity=1):
-        try:
-            product = Product.objects.select_for_update().get(pk=product_id)
-            cls._validate_product(product)
-            cls._validate_quantity(quantity)
+    def add_to_cart(user: User, product_id: int, quantity: int = 1) -> OrderItem:
+        if quantity < 1:
+            raise ValidationError("Количество должно быть положительным.")
 
-            identifier = cls._get_identifier(request)
-            price = product.price * (100 - product.discount) / 100
-
-            item, created = OrderItem.objects.update_or_create(
-                **identifier,
-                product=product,
-                order=None,
-                defaults={'quantity': quantity, 'price': price}
-            )
-
-            return item
-        except ObjectDoesNotExist:
-            raise ProductNotAvailable("Товар не найден")
+        product = Product.objects.get(id=product_id)
+        cart_item, created = OrderItem.objects.get_or_create(
+            user=user,
+            product=product,
+            order__isnull=True,
+            defaults={'quantity': quantity}
+        )
+        if not created:
+            cart_item.quantity += quantity
+            cart_item.save()
+        return cart_item
