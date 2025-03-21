2892625aac81b4b9e64c71b09ef0d9126a76bbcb
NiLNight <bkmz-2020@inbox.ru>
2025-03-21 22:50:31 +0300

    +

--- server/apps/carts/admin.py
+++ server/apps/carts/admin.py
@@ -1,3 +1,16 @@
+# carts/admin.py
 from django.contrib import admin
+from .models import OrderItem
 
-# Register your models here.
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product', 'user', 'quantity', 'order_status')
+    list_filter = ('user', 'product')
+    search_fields = ('product__title', 'user__email')
+    raw_id_fields = ('product', 'user', 'order')
+
+    def order_status(self, obj):
+        return obj.order.status if obj.order else 'В корзине'
+
+    order_status.short_description = 'Статус'
--- server/apps/orders/admin.py
+++ server/apps/orders/admin.py
@@ -1,30 +1,27 @@
-# from django.contrib import admin
-# from apps.orders.models import Order, OrderItem, Delivery
-#
-#
-# class OrderItemInline(admin.TabularInline):
-#     model = OrderItem
-#     extra = 1  # количество пустых форм для добавления новых элементов
-#
-#
-# @admin.register(Order)
-# class OrderAdmin(admin.ModelAdmin):
-#     list_display = ('id', 'user', 'status', 'created')
-#     list_filter = ('status', 'created')
-#     search_fields = ('user__username', 'id')
-#     inlines = [OrderItemInline]
-#     ordering = ('-created',)
-#
-#
-# @admin.register(OrderItem)
-# class OrderItemAdmin(admin.ModelAdmin):
-#     list_display = ('order', 'product', 'quantity', 'price')
-#     list_filter = ('order', 'product')
-#     search_fields = ('order__id', 'product__title')
-#
-#
-# @admin.register(Delivery)
-# class DeliveryAdmin(admin.ModelAdmin):
-#     list_display = ('user', 'address', 'is_primary')
-#     list_filter = ('user', 'is_primary')
-#     search_fields = ('user__username', 'address')
+# orders/admin.py
+from django.contrib import admin
+from .models import Order, Delivery
+from apps.carts.models import OrderItem
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+    fields = ('product', 'quantity')
+    readonly_fields = ('product', 'quantity')
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'status', 'delivery', 'created')
+    list_filter = ('status', 'created')
+    search_fields = ('id', 'user__email')
+    inlines = [OrderItemInline]
+    readonly_fields = ('created', 'updated')
+
+
+@admin.register(Delivery)
+class DeliveryAdmin(admin.ModelAdmin):
+    list_display = ('user', 'address', 'cost', 'is_primary')
+    list_filter = ('is_primary',)
+    search_fields = ('address', 'user__email')
