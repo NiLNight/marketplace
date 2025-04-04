f26088b1c0f17fda0134a2ec442bdbae2552a42d
NiLNight <bkmz-2020@inbox.ru>
2025-04-04 16:12:40 +0300

    Добавлена пагинация для списка заказов

--- server/apps/orders/views.py
+++ server/apps/orders/views.py
@@ -1,5 +1,6 @@
 from rest_framework import status
 from rest_framework.exceptions import ValidationError
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -9,6 +10,12 @@ from apps.orders.serializers import (
     OrderDetailSerializer
 )
 from apps.orders.services import order_services
+
+
+class OrdersPagination(PageNumberPagination):
+    page_size = 20
+    max_page_size = 100
+    page_size_query_param = 'page_size'
 
 
 class OrderCreateView(APIView):
@@ -40,11 +47,17 @@ class OrderCreateView(APIView):
 class OrderListView(APIView):
     permission_classes = [IsAuthenticated]
     serializer_class = OrderSerializer
+    pagination_class = OrdersPagination
 
     def get(self, request):
         orders = order_services.OrderService.get_user_orders(user=request.user)
-        serializer = self.serializer_class(orders, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+
+        # Пагинация
+        paginator = self.pagination_class()
+        page = paginator.paginate_queryset(orders, request)
+
+        serializer = self.serializer_class(page, many=True)
+        return paginator.get_paginated_response(serializer.data)
 
 
 class OrderDetailView(APIView):
