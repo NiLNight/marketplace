944e2f2caff1b3d558bc19ed171e3cd8083066d3
NiLNight <bkmz-2020@inbox.ru>
2025-04-10 18:16:31 +0300

    Сериализаторы для отзывов и комментариев

--- server/apps/reviews/serializers.py
+++ server/apps/reviews/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from apps.reviews.models import Comment
+from apps.reviews.models import Comment, Review
 
 
 class CommentSerializer(serializers.ModelSerializer):
@@ -12,3 +12,23 @@ class CommentSerializer(serializers.ModelSerializer):
 
     def get_children(self, obj):
         return CommentSerializer(obj.get_children(), many=True).data
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    comments = CommentSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Review
+        fields = ['id', 'product', 'user', 'value', 'text', 'created', 'comments']
+
+
+class ReviewCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = ['product', 'value', 'text']
+
+
+class CommentCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['review', 'text', 'parent']
