e3c319f96627636438c092cc14e6ba99878152ed
NiLNight <bkmz-2020@inbox.ru>
2025-04-12 14:05:04 +0300

    Админка

--- server/apps/reviews/admin.py
+++ server/apps/reviews/admin.py
@@ -1,9 +1,5 @@
 from django.contrib import admin
-from .models import Review
+from apps.reviews.models import Review
 
 
-@admin.register(Review)
-class RatingAdmin(admin.ModelAdmin):
-    list_display = ('product', 'user', 'value', 'created', 'ip_address')
-    list_filter = ('value', 'created', 'updated')
-    search_fields = ('product__title', 'user__username', 'ip_address')
+admin.site.register(Review)
