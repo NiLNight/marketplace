d63c446b6e67dff51cb9298641efffb613bdb3b1
NiLNight <bkmz-2020@inbox.ru>
2025-04-14 19:55:54 +0300

    Обновлены сериалайзеры (бета-версия)

--- server/apps/reviews/serializers.py
+++ server/apps/reviews/serializers.py
@@ -8,27 +8,31 @@ class CommentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Comment
-        fields = ['id', 'review', 'user', 'text', 'create_time', 'parent', 'children']
+        fields = ['id', 'review', 'user', 'text', 'created', 'parent', 'children']
 
     def get_children(self, obj):
         return CommentSerializer(obj.get_children(), many=True).data
-
-
-class ReviewSerializer(serializers.ModelSerializer):
-    comments = CommentSerializer(many=True, read_only=True)
-
-    class Meta:
-        model = Review
-        fields = ['id', 'product', 'user', 'value', 'text', 'created', 'comments']
-
-
-class ReviewCreateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Review
-        fields = ['product', 'value', 'text']
 
 
 class CommentCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = ['review', 'text', 'parent']
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    comments = CommentSerializer(many=True, read_only=True)
+    likes_count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Review
+        fields = ['id', 'product', 'user', 'value', 'text', 'created', 'comments', 'likes_count']
+
+    def get_likes_count(self, obj):
+        return obj.likes.count()
+
+
+class ReviewCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = ['product', 'value', 'text', 'image']
