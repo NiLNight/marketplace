79c746f678724d9c4e67a4ede20b3316d9794543
NiLNight <bkmz-2020@inbox.ru>
2025-04-21 19:12:08 +0300

    Маршруты

--- server/apps/wishlists/urls.py
+++ server/apps/wishlists/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from apps.wishlists.views import (
     WishlistAddView,
+    WishlistItemDeleteView,
 )
 
 urlpatterns = [
-    path('add/', WishlistAddView.as_view(), name='wishlist-add')
+    path('add/', WishlistAddView.as_view(), name='wishlist-add'),
+    path('delete/<int:pk>/', WishlistItemDeleteView.as_view(), name='wishlist-item-delete')
 ]
