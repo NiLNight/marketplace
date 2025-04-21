ae924d8865a79e95f278ef3a93196db38a32087a
NiLNight <bkmz-2020@inbox.ru>
2025-04-21 17:31:23 +0300

    Представление для добавления товара в список желаний

--- server/apps/wishlists/views.py
+++ server/apps/wishlists/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from apps.wishlists.exceptions import ProductNotAvailable
+from apps.wishlists.serializers import WishlistItemSerializer
+from apps.wishlists.services.wishlist_services import WishlistService
+
+
+class WishlistAddView(APIView):
+    permission_classes = [AllowAny]
+    serializer_class = WishlistItemSerializer
+
+    def post(self, request):
+        try:
+            product_id = int(request.data['product_id'])
+            WishlistService.add_to_wishlist(request, product_id)
+            return Response({"message": "Товар добавлен в список желаний"}, status=status.HTTP_200_OK)
+        except (ValueError, TypeError):
+            return Response({"error": "Некорректный ID товара"}, status=status.HTTP_400_BAD_REQUEST)
+        except ProductNotAvailable as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
