993c2385149b5ae93e6c322a6afda75345e20e9b
NiLNight <bkmz-2020@inbox.ru>
2025-05-01 15:08:06 +0300

    +

--- server/config/urls.py
+++ server/config/urls.py
@@ -6,7 +6,6 @@ from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 from rest_framework_simplejwt.views import TokenRefreshView
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('apps.users.urls')),
