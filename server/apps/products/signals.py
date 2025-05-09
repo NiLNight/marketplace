88b6da14006ec131373973ccad4805eabbba964f
NiLNight <bkmz-2020@inbox.ru>
2025-05-09 17:28:35 +0300

    Сигнал для вызова обновления elasticsearch

--- server/apps/products/signals.py
+++ server/apps/products/signals.py
@@ -1,36 +1,20 @@
 import logging
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 from apps.products.models import Product
-from apps.products.documents import ProductDocument
+from apps.products.services.tasks import update_elasticsearch_task
 
 logger = logging.getLogger(__name__)
 
 
 @receiver(post_save, sender=Product)
 def update_product_in_elasticsearch(sender, instance, created, **kwargs):
-    """Обновляет или создаёт документ в Elasticsearch при сохранении продукта."""
+    """Запускает асинхронную задачу для Elasticsearch."""
     user_id = instance.user.id if instance.user else 'anonymous'
     action = 'Creating' if created else 'Updating'
-    logger.info(
-        f"{action} product in Elasticsearch with title={instance.title}, user={user_id}, is_active={instance.is_active}")
-    try:
-        if instance.should_update_elasticsearch():
-            ProductDocument().update(instance)
-            logger.info(f"Successfully {action.lower()} product in Elasticsearch {instance.id}, user={user_id}")
-        else:
-            logger.info(f"Skipping Elasticsearch update for product {instance.id}, user={user_id}")
-    except Exception as e:
-        logger.exception(f"Failed to {action.lower()} product in Elasticsearch {instance.id}: {str(e)}, user={user_id}")
-
-
-@receiver(post_delete, sender=Product)
-def delete_product_from_elasticsearch(sender, instance, **kwargs):
-    """Удаляет документ из Elasticsearch при удалении продукта."""
-    user_id = instance.user.id if instance.user else 'anonymous'
-    logger.info(f"Deleting product from Elasticsearch with title={instance.title}, user={user_id}")
-    try:
-        ProductDocument().delete(instance)
-        logger.info(f"Successfully deleted product from Elasticsearch {instance.id}, user={user_id}")
-    except Exception as e:
-        logger.exception(f"Failed to delete product from Elasticsearch {instance.id}: {str(e)}, user={user_id}")
+    # Пропускаем сигнал, если обновляется только popularity_score
+    if kwargs.get('update_fields') == {'popularity_score'}:
+        logger.debug(f"Skipping signal for product {instance.id} due to popularity_score update")
+        return
+    logger.info(f"{action} product: title={instance.title}, user={user_id}, is_active={instance.is_active}")
+    update_elasticsearch_task.delay(instance.id)
