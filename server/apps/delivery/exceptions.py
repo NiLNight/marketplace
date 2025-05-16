4e50699c1024d124dbc8b9bfe4cb6e1219ea2c3a
NiLNight <bkmz-2020@inbox.ru>
2025-05-16 21:44:44 +0300

    Кастомные ошибки

--- server/apps/delivery/exceptions.py
+++ server/apps/delivery/exceptions.py
@@ -2,6 +2,13 @@ from rest_framework.exceptions import APIException
 from django.utils.translation import gettext_lazy as _
 
 
+class DeliveryServiceException(APIException):
+    """Базовое исключение для операций с доставкой."""
+    status_code = 400
+    default_detail = _("Ошибка при обработке доставки")
+    default_code = "delivery_service_error"
+
+
 class DeliveryNotFound(APIException):
     """Исключение, вызываемое при отсутствии адреса доставки."""
     status_code = 404
@@ -21,3 +28,10 @@ class CityNotFound(APIException):
     status_code = 404
     default_detail = _("Город не найден")
     default_code = "city_not_found"
+
+
+class ElasticsearchUnavailable(APIException):
+    """Исключение, вызываемое при недоступности Elasticsearch."""
+    status_code = 503
+    default_detail = _("Сервис поиска временно недоступен")
+    default_code = "service_unavailable"
