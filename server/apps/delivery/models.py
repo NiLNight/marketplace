c326b26ac98d89718154091597e36a52dbccac4b
NiLNight <bkmz-2020@inbox.ru>
2025-05-22 17:10:40 +0300

    Удаление модели доставок

--- server/apps/delivery/models.py
+++ server/apps/delivery/models.py
@@ -1,72 +1,38 @@
 from django.db import models
-from django.contrib.auth import get_user_model
 from django.core.validators import MinLengthValidator
 from django.contrib.postgres.search import SearchVectorField, SearchVector, Value
 from django.utils.translation import gettext_lazy as _
 
-User = get_user_model()
-
 
 class City(models.Model):
+    """
+    Модель для хранения городов.
+
+    Attributes:
+        name (CharField): Название города.
+    """
     name = models.CharField(
         max_length=100,
         unique=True,
-        verbose_name='Название города'
+        verbose_name=_('Название')
     )
 
     class Meta:
-        indexes = [
-            models.Index(fields=['name']),
-        ]
-        verbose_name = 'Город'
-        verbose_name_plural = 'Города'
+        """Метаданные модели City."""
+        ordering = ['name']  # Сортировка по имени города
+        verbose_name = _('Город')
+        verbose_name_plural = _('Города')
 
     def __str__(self) -> str:
+        """
+        Строковое представление города.
+
+        Returns:
+            str: Название города.
+        """
         return self.name
 
     def save(self, *args, **kwargs):
-        self.full_clean()
-        super().save(*args, **kwargs)
-
-
-class Delivery(models.Model):
-    user = models.ForeignKey(
-        User,
-        related_name='deliveries',
-        on_delete=models.CASCADE,
-        verbose_name='Пользователь'
-    )
-    address = models.CharField(
-        max_length=255,
-        null=True,
-        blank=True,
-        validators=[MinLengthValidator(5, message="Адрес должен содержать не менее 5 символов")],
-        verbose_name='Адрес'
-    )
-    cost = models.DecimalField(
-        max_digits=10,
-        decimal_places=2,
-        default=0,
-        verbose_name='Стоимость'
-    )
-    is_primary = models.BooleanField(
-        default=False,
-        verbose_name='Основной адрес'
-    )
-
-    class Meta:
-        indexes = [
-            models.Index(fields=['user', 'is_primary']),
-        ]
-        verbose_name = 'Адрес доставки'
-        verbose_name_plural = 'Адреса доставки'
-
-    def __str__(self) -> str:
-        return f"Адрес доставки для пользователя {self.user.username}"
-
-    def save(self, *args, **kwargs):
-        if self.is_primary:
-            Delivery.objects.filter(user=self.user, is_primary=True).exclude(pk=self.pk).update(is_primary=False)
         self.full_clean()
         super().save(*args, **kwargs)
 
@@ -82,6 +48,12 @@ class PickupPoint(models.Model):
         max_length=255,
         validators=[MinLengthValidator(5, message="Адрес должен содержать не менее 5 символов")],
         verbose_name='Адрес'
+    )
+    district = models.CharField(
+        max_length=100,
+        null=True,
+        blank=True,
+        verbose_name='Район'
     )
     is_active = models.BooleanField(
         default=True,
@@ -105,9 +77,11 @@ class PickupPoint(models.Model):
 
     def save(self, *args, **kwargs):
         city_name = self.city.name if self.city else ''
+        district_name = self.district or ''
         self.search_vector = (
                 SearchVector(Value(self.address), weight='A', config='russian') +
-                SearchVector(Value(city_name), weight='B', config='russian')
+                SearchVector(Value(city_name), weight='B', config='russian') +
+                SearchVector(Value(district_name), weight='B', config='russian')
         )
         self.full_clean()
         super().save(*args, **kwargs)
