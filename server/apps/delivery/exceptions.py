406478edc694aa004a793ad918e0d326e5d56562
NiLNight <bkmz-2020@inbox.ru>
2025-05-22 16:59:10 +0300

    Удаление ошибок для доставок

--- server/apps/delivery/exceptions.py
+++ server/apps/delivery/exceptions.py
@@ -2,20 +2,6 @@ from rest_framework.exceptions import APIException
 from django.utils.translation import gettext_lazy as _
 
 
-class DeliveryServiceException(APIException):
-    """Базовое исключение для операций с доставкой."""
-    status_code = 400
-    default_detail = _("Ошибка при обработке доставки")
-    default_code = "delivery_service_error"
-
-
-class DeliveryNotFound(APIException):
-    """Исключение, вызываемое при отсутствии адреса доставки."""
-    status_code = 404
-    default_detail = _("Адрес доставки не найден")
-    default_code = "delivery_not_found"
-
-
 class PickupPointNotFound(APIException):
     """Исключение, вызываемое при отсутствии или неактивности пункта выдачи."""
     status_code = 404
