f52c1165544c1681bc52b4ed3c8c82a2eb3d5d50
NiLNight <bkmz-2020@inbox.ru>
2025-05-22 17:05:36 +0300

    Удаление доставок из сериализаторов

--- server/apps/orders/serializers.py
+++ server/apps/orders/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from django.utils.translation import gettext_lazy as _
 from apps.orders.models import Order
 from apps.carts.serializers import CartItemSerializer
-from apps.delivery.serializers import DeliverySerializer, PickupPointSerializer
+from apps.delivery.serializers import PickupPointSerializer
 
 
 class OrderSerializer(serializers.ModelSerializer):
@@ -10,21 +10,20 @@ class OrderSerializer(serializers.ModelSerializer):
     Сериализатор для списка заказов.
 
     Преобразует объекты Order в JSON, включая основные поля заказа для отображения в списке.
-    Проверяет корректность статуса, общей стоимости и выбора доставки/пункта выдачи.
+    Проверяет корректность статуса, общей стоимости и пункта выдачи.
     """
-    delivery = DeliverySerializer(read_only=True)
     pickup_point = PickupPointSerializer(read_only=True)
 
     class Meta:
         """Метаданные сериализатора OrderSerializer."""
         model = Order
-        fields = ['id', 'status', 'total_price', 'created', 'delivery', 'pickup_point']
+        fields = ['id', 'status', 'total_price', 'created', 'pickup_point']
 
     def validate(self, attrs):
         """
         Проверяет корректность данных перед сериализацией.
 
-        Убеждается, что статус валиден, total_price неотрицателен, и указан либо delivery, либо pickup_point.
+        Убеждается, что статус валиден, total_price неотрицателен, и указан активный pickup_point.
 
         Args:
             attrs (dict): Данные для сериализации.
@@ -41,14 +40,10 @@ class OrderSerializer(serializers.ModelSerializer):
                 raise serializers.ValidationError({"status": _("Недопустимый статус заказа")})
             if instance.total_price < 0:
                 raise serializers.ValidationError({"total_price": _("Общая стоимость не может быть отрицательной")})
-            if instance.delivery and instance.pickup_point:
-                raise serializers.ValidationError(
-                    {"delivery": _("Нельзя указать и доставку, и пункт выдачи одновременно")}
-                )
-            if not instance.delivery and not instance.pickup_point:
-                raise serializers.ValidationError(
-                    {"delivery": _("Необходимо указать либо доставку, либо пункт выдачи")}
-                )
+            if not instance.pickup_point:
+                raise serializers.ValidationError({"pickup_point": _("Пункт выдачи обязателен")})
+            if not instance.pickup_point.is_active:
+                raise serializers.ValidationError({"pickup_point": _("Пункт выдачи неактивен!")})
         return attrs
 
 
@@ -56,7 +51,7 @@ class OrderDetailSerializer(serializers.ModelSerializer):
     """
     Сериализатор для детального отображения заказа.
 
-    Преобразует объекты Order в JSON, включая элементы заказа, доставку и пункт выдачи.
+    Преобразует объекты Order в JSON, включая элементы заказа и пункт выдачи.
     Проверяет корректность статуса, общей стоимости и связанных данных.
     """
     items = CartItemSerializer(
@@ -65,19 +60,18 @@ class OrderDetailSerializer(serializers.ModelSerializer):
         read_only=True,
         help_text='Список элементов заказа из корзины.'
     )
-    delivery = DeliverySerializer(read_only=True)
     pickup_point = PickupPointSerializer(read_only=True)
 
     class Meta:
         """Метаданные сериализатора OrderDetailSerializer."""
         model = Order
-        fields = ['id', 'status', 'total_price', 'created', 'items', 'delivery', 'pickup_point']
+        fields = ['id', 'status', 'total_price', 'created', 'items', 'pickup_point']
 
     def validate(self, attrs):
         """
         Проверяет корректность данных перед сериализацией.
 
-        Убеждается, что статус валиден, total_price неотрицателен, delivery/pickup_point активны,
+        Убеждается, что статус валиден, total_price неотрицателен, pickup_point активен,
         и все товары активны.
 
         Args:
@@ -95,15 +89,9 @@ class OrderDetailSerializer(serializers.ModelSerializer):
                 raise serializers.ValidationError({"status": _("Недопустимый статус заказа")})
             if instance.total_price < 0:
                 raise serializers.ValidationError({"total_price": _("Общая стоимость не может быть отрицательной")})
-            if not instance.delivery and not instance.pickup_point:
-                raise serializers.ValidationError(
-                    {"delivery": _("Необходимо указать либо доставку, либо пункт выдачи")}
-                )
-            if instance.delivery and instance.pickup_point:
-                raise serializers.ValidationError(
-                    {"delivery": _("Нельзя указать и доставку, и пункт выдачи одновременно")}
-                )
-            if instance.pickup_point and not instance.pickup_point.is_active:
+            if not instance.pickup_point:
+                raise serializers.ValidationError({"pickup_point": _("Пункт выдачи обязателен!")})
+            if not instance.pickup_point.is_active:
                 raise serializers.ValidationError({"pickup_point": _("Пункт выдачи неактивен")})
             for item in instance.order_items.all():
                 if not item.product.is_active:
