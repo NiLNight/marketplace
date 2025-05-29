253ab9a467cda1b788df77535be4bc798ec932cc
NiLNight <bkmz-2020@inbox.ru>
2025-05-29 16:27:06 +0300

    Создание файла тестов в приложении Carts

--- server/apps/carts/apps.py
+++ server/apps/carts/apps.py
@@ -2,5 +2,13 @@ from django.apps import AppConfig
 
 
 class CartsConfig(AppConfig):
+    """Конфигурация приложения carts.
+
+    Определяет основные настройки приложения для управления корзинами пользователей.
+
+    Attributes:
+        default_auto_field (str): Тип поля для автоматически создаваемых первичных ключей.
+        name (str): Полное имя приложения.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.carts'
