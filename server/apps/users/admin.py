cdea64c270147ad6f27ffc19184983a1c2449a65
NiLNight <bkmz-2020@inbox.ru>
2025-05-29 16:17:47 +0300

    Обновление файла admin.py согласно правилам в приложении Users

--- server/apps/users/admin.py
+++ server/apps/users/admin.py
@@ -5,6 +5,18 @@ from apps.users.models import UserProfile
 
 
 class UserProfileInline(admin.StackedInline):
+    """Инлайн-класс для отображения профиля пользователя в админке.
+
+    Позволяет редактировать данные профиля непосредственно на странице пользователя.
+
+    Attributes:
+        model: Модель профиля пользователя (UserProfile).
+        can_delete (bool): Запрещает удаление профиля через админку.
+        verbose_name (str): Название в единственном числе.
+        verbose_name_plural (str): Название во множественном числе.
+        fields (tuple): Поля для отображения и редактирования.
+        readonly_fields (tuple): Поля, доступные только для чтения.
+    """
     model = UserProfile
     can_delete = False
     verbose_name = 'Профиль'
@@ -19,6 +31,14 @@ class UserProfileInline(admin.StackedInline):
 
 
 class CustomUserAdmin(UserAdmin):
+    """Кастомный администраторский класс для модели пользователя.
+
+    Расширяет стандартный UserAdmin, добавляя инлайн для профиля и кастомные поля в списке.
+
+    Attributes:
+        inlines (tuple): Инлайн-классы для отображения связанных данных.
+        list_display (tuple): Поля для отображения в списке пользователей.
+    """
     inlines = (UserProfileInline,)
     list_display = (
         'username',
@@ -29,11 +49,27 @@ class CustomUserAdmin(UserAdmin):
     )
 
     def get_phone(self, obj):
+        """Возвращает номер телефона из профиля пользователя.
+
+        Args:
+            obj (User): Объект пользователя.
+
+        Returns:
+            str: Номер телефона или дефис, если профиль отсутствует.
+        """
         return obj.profile.phone if hasattr(obj, 'profile') else '-'
 
     get_phone.short_description = 'Телефон'
 
     def get_avatar(self, obj):
+        """Возвращает аватар из профиля пользователя.
+
+        Args:
+            obj (User): Объект пользователя.
+
+        Returns:
+            str: URL аватара или дефис, если профиль отсутствует.
+        """
         return obj.profile.avatar if hasattr(obj, 'profile') else '-'
 
     get_avatar.short_description = 'Аватар'
@@ -41,6 +77,16 @@ class CustomUserAdmin(UserAdmin):
 
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
+    """Администраторский класс для модели профиля пользователя.
+
+    Настраивает отображение, поиск и фильтрацию профилей в админке.
+
+    Attributes:
+        list_display (tuple): Поля для отображения в списке профилей.
+        search_fields (tuple): Поля для поиска.
+        list_filter (tuple): Поля для фильтрации.
+        readonly_fields (tuple): Поля, доступные только для чтения.
+    """
     list_display = (
         'user',
         'public_id',
@@ -57,6 +103,14 @@ class UserProfileAdmin(admin.ModelAdmin):
     readonly_fields = ('avatar_tag', 'public_id')
 
     def avatar_tag(self, obj):
+        """Возвращает HTML-тег для предпросмотра аватара.
+
+        Args:
+            obj (UserProfile): Объект профиля пользователя.
+
+        Returns:
+            str: HTML-тег с изображением или дефис, если аватар отсутствует.
+        """
         from django.utils.html import format_html
         return format_html(
             '<img src="{}" style="max-height: 50px; max-width: 50px;" />',
