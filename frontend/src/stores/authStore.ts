7ea5c1f03751411a913c37222ad69784a9cdfe51
NiLNight <bkmz-2020@inbox.ru>
2025-06-28 13:41:17 +0300

    изменено время обновления токена

--- frontend/src/stores/authStore.ts
+++ frontend/src/stores/authStore.ts
@@ -23,7 +23,7 @@ interface AuthState {
 
 let refreshIntervalId: number | null = null;
 // Обновляем токен за 30 секунд до его истечения (в вашем случае 2 минуты)
-const REFRESH_INTERVAL = (2 * 60 - 30) * 1000; // 90 секунд
+const REFRESH_INTERVAL = (15 * 60 - 30) * 1000; // 90 секунд
 
 const startProactiveTokenRefresh = () => {
     if (refreshIntervalId) {
