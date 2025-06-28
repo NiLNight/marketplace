04e4d4ea9aa29ae90252e8338fa870afd074a816
NiLNight <bkmz-2020@inbox.ru>
2025-06-28 11:59:08 +0300

    Использование кастомного представления в маршруте обновления токена

--- server/config/urls.py
+++ server/config/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
-from rest_framework_simplejwt.views import TokenRefreshView
+from apps.users.views import CustomTokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -19,7 +19,7 @@ urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger'),
     path('api/docs/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 if settings.DEBUG:
