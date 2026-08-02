81934585f7e9dacd66f643c52d26f9f2cd0e5dfd
NiLNight <bkmz-2020@inbox.ru>
2026-08-02 17:56:20 +0300

    Перенос настроек почтового сервиса в env

--- server/config/settings.py
+++ server/config/settings.py
@@ -222,11 +222,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Конфигурация сервера электронной почты
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = str(os.environ.get('EMAIL_HOST'))
 EMAIL_HOST_USER = str(os.environ.get('EMAIL_HOST_USER'))
 EMAIL_HOST_PASSWORD = str(os.environ.get('EMAIL_HOST_PASSWORD'))
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = str(os.environ.get('EMAIL_PORT'))
+EMAIL_USE_TLS = str(os.environ.get('EMAIL_USE_TLS'))
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # CORS настройки
